25aae82513bfd97dc2f64879b7d47dabf4564bdd
fajryhamzah <fajryhamzah@gmail.com>
2016-03-21 10:29:26 +0700

    fh-smooth: smooth scrolling plugin

---
+++ fh-smooth/fh-smooth.js
@@ -0,0 +1,22 @@
+/*!
+ * fh-smooth v1.0.0 (http://www.fajryhamzah.com)
+ * Copyright 2016
+ * Licensed under the MIT license
+ */
+$.fn.fhsmooth = function(options){
+  var size=0;
+  var setting = $.extend({
+    duration:1000
+  },options);
+
+  $(this).click(function(e){
+  e.preventDefault();
+  var target = $(this).attr("href");
+  size += $(target).position().top;
+
+  $("body").animate({
+      scrollTop: size
+    },setting.duration); 
+
+  });
+};
