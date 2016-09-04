b1c75e4ae66c58a16668bfe4ec4ed4d92ff3f65f
Fajry Hamzah <fajryhamzah@users.noreply.github.com>
2016-09-04 21:09:13 +0700

    Update fh-smooth.js

--- fh-smooth/fh-smooth.js
+++ fh-smooth/fh-smooth.js
@@ -3,21 +3,24 @@
  * Copyright 2016
  * Licensed under the MIT license
  */
-$.fn.fhsmooth = function(options){
-  var size=0;
-  var setting = $.extend({
-    duration:1000
-  },options);
 
-  $(this).click(function(e){
-  e.preventDefault();
-  var target = $(this).attr("href");
-  if(target == "#") target = "body";
-  size = $(target).position().top;
+(function ( $ ) {
+  $.fn.fhsmooth = function(options){
+    var size=0;
+    var setting = $.extend({
+      duration:1000
+    },options);
 
-  $("html,body").animate({
-      scrollTop: size
-    },setting.duration); 
+    $(this).click(function(e){
+    e.preventDefault();
+    var target = $(this).attr("href");
+    if(target == "#") target = "body";
+    size = $(target).position().top;
 
-  });
-};
+    $("html,body").animate({
+        scrollTop: size
+      },setting.duration); 
+
+    });
+  };
+}( jQuery ));
